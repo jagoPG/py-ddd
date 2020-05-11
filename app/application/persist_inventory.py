9a01aba4b6940c07c8539c46558de27441a3cf57
Jagoba Pérez-Gómez <jagobapg@pm.me>
2020-05-11 12:26:53 +0200

    Add persist action and update service list

---
+++ app/application/persist_inventory.py
@@ -0,0 +1,25 @@
+from app.domain.model.Event import EventId
+from app.domain.model.Inventory import Inventory, InventoryId, InventoryAmount, SellerName
+
+
+class PersistInventoryCommand:
+    def __init__(self, identifier, event_id, amount, seller_name):
+        self.identifier = identifier
+        self.event_id = event_id
+        self.amount = amount
+        self.seller_name = seller_name
+
+
+class PersistInventory:
+    def __init__(self, repository):
+        self.repository = repository
+
+    def handle(self, command):
+        self.repository.persist(
+            Inventory(
+                InventoryId(command.identifier),
+                EventId(command.identifier),
+                InventoryAmount(command.amount),
+                SellerName(command.seller_name)
+            )
+        )
